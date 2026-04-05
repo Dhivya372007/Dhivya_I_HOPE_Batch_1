33e6e469f3e2c4ed12b006fc1821f66ba36d859b
Dhivya372007 <dhivya372007@gmail.com>
2026-04-05 19:41:44 +0530

    ArrayListImplementation.java

---
+++ Codebase/ArrayListImplementation.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ArrayListImplementation {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList=new ArrayList<>();
+        arrayList.add(null);
+        arrayList.add(10);
+        arrayList.add(20);
+        System.out.println(arrayList);
+        arrayList.set(0,5);
+        System.out.println(arrayList);
+        arrayList.set(0,0);
+        System.out.println(arrayList);
+        arrayList.remove(2);
+        System.out.println(arrayList);
+        System.out.println(arrayList.lastIndexOf(0));
+        System.out.println(arrayList.isEmpty());
+        arrayList.clear();
+        System.out.println(arrayList);
+        System.out.println(arrayList.isEmpty());
+        
+
+    }
+}
